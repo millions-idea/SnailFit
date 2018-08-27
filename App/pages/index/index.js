92befdb39813e3b59ce15931144f737a178c77e5
root <2407439@qq.com>
2018-08-27 17:30:20 +0800

    完成首页布局设计

--- App/pages/index/index.js
+++ App/pages/index/index.js
@@ -4,51 +4,118 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    imgUrls: [
+      '/dist/images/index/banner.png',
+      '/dist/images/index/banner.png',
+      '/dist/images/index/banner.png'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    indicatorColor: "#FFF",
+    indicatorActiveColor: "#FD8101",
+    // 下拉面板交互属性
+    sortSelectPanelIndex: 0,
+    activeIndex: 0,
+    toView: 'red',
+    // 底部弹窗交互属性
+    msgDisplay: "block",
+    // 收藏栏目交互属性
+    isCollect: false
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  /**
+   * 展开排序下拉框
+   */
+  sortDropdown: function(){
+    var that = this;
+    if (that.data.sortSelectPanelIndex != 1){
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        sortSelectPanelIndex: 1,
+        activeIndex: 1
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+    }else{
+      this.setData({
+        sortSelectPanelIndex: 0,
+        activeIndex: 0
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  /**
+   * 展开门店下拉框
+   */
+  shopDropdown: function () {
+    var that = this;
+    if (that.data.sortSelectPanelIndex != 2) {
+      this.setData({
+        sortSelectPanelIndex: 2,
+        activeIndex: 2
+      })
+    }else{
+      this.setData({
+        sortSelectPanelIndex: 0,
+        activeIndex: 0
+      })
+    }
+  },
+  /**
+   * 关闭底部弹窗
+   */
+  closeMsg: function(){
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      msgDisplay: "none",
     })
+  },
+  /**
+   * 收藏栏目
+   */
+  collect: function(){
+    // TODO 实际接入数据后请根据list中的值进行双向绑定
+    var that = this;
+    if (that.data.isCollect == false){
+      this.setData({
+        isCollect: true
+      })
+    }else{
+      this.setData({
+        isCollect: false
+      })
+    }
+    
   }
 })
